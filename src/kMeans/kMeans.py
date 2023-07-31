05d50ac3314ea2b9ea946bd7a611a8efbea7ea55
psicobloc <valenciavhugo@gmail.com>
2023-07-30 21:55:18 -0500

    chance lo arruine mas

--- src/kMeans/kMeans.py
+++ src/kMeans/kMeans.py
@@ -2,68 +2,58 @@ import sys
 import numpy as np 
 import cv2 as cv
 
-
-
-
 class k_means:
-    def __init__(self, k: int, path: str, max_iters = 14):
-        self.k = k
-        self.imgPath = path 
+    def __init__(self, arg):
+        self.k = int(arg[1])
+        self.imgPath = arg[2] 
+        self.max_iters = int(arg[3])
         # load image - 3D Array (r,g,b)
-        self.originalImg = np.asarray(cv.imread(path))
+        # self.originalImg = 
+        self.X = np.asarray(cv.imread(self.imgPath))
         # calculate centroids
-        self.centroids = self.initCentroids(self.k, self.originalImg)
+        self.centroids = np.zeros((self.k, np.size(self.X, 1)))
+        self.initCentroids()
 
+    def initCentroids(self):
+        self.idx = np.random.choice(self.X.shape[0], self.k, replace=False)
+        self.centroids = self.X[self.idx]
 
-    def initCentroids (k,  mat):
-        centroids = np.zeros(k, np.size(mat, 1))
-        centroidIndexes = np.random.choice(mat.shape[0], k, replace=False)
-        centroids = mat[centroidIndexes]
-        return centroids
-
-    def findClosestCentroids(X, centroids, k):
+    def findClosestCentroids(self):
         # index of the centroid assigned to example i
-        idx = np.zeros(np.shape(X)[0], 1)
+        self.idx = np.zeros((np.shape(self.X)[0], 1))
         # si tomamos la diferencia entre un ejemplo contra todas las clases
         # idx sera el indice del centroide con la menor diferencia
         # podeos vectorizar y sacar las k distancias de cada ejemplo al mismo tiempo
         # y luego tomamos solo la distancia menor, el indice de esa distancia menor esra idx
-        for i in range(np.shape(X)[0]):
-            idx[i] = np.argmin(np.sum(np.square(X[i,:] - centroids)))
-        return idx
+        for i in range(np.shape(self.X)[0]):
+            self.idx[i] = np.argmin(np.sum(np.square(self.X[i,:] - self.centroids)))
 
+    def computeCentroids(self):
+        for iter in range(int(self.k)):
+            indicesCentroide = (self.idx == iter).nonzero()
+            print(indicesCentroide)
+            sumaX = self.X[indicesCentroide,:].sum(axis=1)
+            cuentaIndices = len(indicesCentroide[0])
+            efe = (1/cuentaIndices)*sumaX
+            ge = efe[0,:,:]
+            print(iter)
+            self.centroids[iter,:] = ge #(1/cuentaIndices)*sumaX #movemos el centroide al promedio
 
+    def run(self):
+        for i in range(self.max_iters):
+            print(f"K-means iteration: {i} - max: {self.max_iters}")
+            self.findClosestCentroids()
+            self.computeCentroids()
 
-
-    def runKmeans(X, initial_centroids, max_iters):
-        rows, columns = np.shape(X)
-        previous_centroids = centroids = initial_centroids
-        idx = np.zeros((rows, 1))
-        for i in range(max_iters):
-            print(f"K-means iteration: {i} - max: {max_iters}")
-            idx = self.findClosestCentroids(X, centroids)
-
-    
-
-
-
-
-
-
-
-
-
-
-
-def main (k, path):
-    k_means_instance = k_means(k, path)
-
+def main (args):
+    k_means_instance = k_means(args)
+    k_means_instance.run()
+    centroids = k_means_instance.centroids
+    k_means_instance.findClosestCentroids()
+    x_recovered = k_means_instance.centroids[k_means_instance.idx, :]
+    cv.imwrite('recovered.jpg', x_recovered)
     
 if __name__ == '__main__':
-    k = int(sys.argv[0]) # size of K
-    img_path = sys.argv[1] # path to image
-
-
-
-
-    main()
+    args = sys.argv
+    # k, img path, max_iters
+    main(args)
